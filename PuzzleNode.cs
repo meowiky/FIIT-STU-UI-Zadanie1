da9596789d06e464affae69455cd15da7ae521e2
meowiky <xbukovska@stuba.sk>
2023-09-30 21:35:26 +0200

    added new constructor, method for returning new state after move and new position for space

--- PuzzleNode.cs
+++ PuzzleNode.cs
@@ -8,19 +8,19 @@ namespace UI_Zadanie1_c_Bukovska
 {
     public class PuzzleNode
     {
-        public int[,] State{get; set;}
+        public int[,] State { get; set; }
         public List<MoveEnum> Moves { get; set; }
-        public int[] SpaceIndex { get; set; }
+        public int[] SpacePosition { get; set; }
 
         public PuzzleNode()
         {
-            State = new int[3,3];
+            State = new int[3, 3];
             Moves = new List<MoveEnum>();
-            SpaceIndex = new int[2];
+            SpacePosition = new int[2];
         }
 
         public PuzzleNode(int[] input)
-            :this() 
+            : this()
         {
             int index = 0;
             for (int row = 0; row < 3; row++)
@@ -30,17 +30,66 @@ namespace UI_Zadanie1_c_Bukovska
                     State[row, col] = input[index];
                     if (input[index] == 0)
                     {
-                        SpaceIndex[0] = row;
-                        SpaceIndex[0] = col;
+
+                        SpacePosition[0] = row;
+                        SpacePosition[0] = col;
                     }
                     index++;
                 }
             }
         }
 
+        public PuzzleNode(int[,] state, int[] spacePosition)
+
+
+
         public List<MoveEnum> GetPossibleMoves()
         {
-            return PossibleMoves.possibleMoves[(SpaceIndex[0], SpaceIndex[0])];
+            return PossibleMoves.possibleMoves[(SpacePosition[0], SpacePosition[0])];
+        }
+
+        public (int[,], int[]) GetNewState(MoveEnum move)
+        {
+            int[,] newState = new int[3,3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    newState[i, j] = State[i, j];
+                }
+            }
+
+            int tomoveX = -1;
+            int tomoveY = -1;
+
+            switch (move) 
+            {
+                case MoveEnum.Up:
+                    tomoveX = SpacePosition[0] + 1;
+                    tomoveY = SpacePosition[1];
+                    break;
+                case MoveEnum.Down:
+                    tomoveX = SpacePosition[0] - 1;
+                    tomoveY = SpacePosition[1];
+                    break;
+                case MoveEnum.Left:
+                    tomoveX = SpacePosition[0];
+                    tomoveY = SpacePosition[1] - 1;
+                    break;
+                case MoveEnum.Right:
+                    tomoveX = SpacePosition[0];
+                    tomoveY = SpacePosition[1] + 1;
+                    break;
+            }
+
+            newState[SpacePosition[0], SpacePosition[1]] = State[tomoveX, tomoveY];
+            newState[tomoveX, tomoveY] = State[SpacePosition[0], SpacePosition[1]];
+
+            int[] spacePosition = new int[2];
+            spacePosition[0] = tomoveX;
+            spacePosition[1] = tomoveY;
+
+            return (newState, spacePosition);
         }
     }
 }

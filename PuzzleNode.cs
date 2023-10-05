4bb132a0f920d7c49a8191f2fe317338949be9ab
meowiky <xbukovska@stuba.sk>
2023-10-05 12:34:58 +0200

    changed so it can be m*n puzzle, changed get possible moves, deleted class possible moves cause its useless

--- PuzzleNode.cs
+++ PuzzleNode.cs
@@ -8,24 +8,26 @@ namespace UI_Zadanie1_c_Bukovska
 {
     public class PuzzleNode
     {
+        public int Rows { get; private set; }
+        public int Columns { get; private set; }
         public int[,] State { get; set; }
         public List<MoveEnum> Moves { get; set; }
         public int[] SpacePosition { get; set; }
 
-        public PuzzleNode()
+        public PuzzleNode(int rows, int columns)
         {
-            State = new int[3, 3];
+            State = new int[rows, columns];
             Moves = new List<MoveEnum>();
             SpacePosition = new int[2];
         }
 
-        public PuzzleNode(int[] input)
-            : this()
+        public PuzzleNode(int[] input, int rows, int columns)
+            : this(rows, columns)
         {
             int index = 0;
-            for (int row = 0; row < 3; row++)
+            for (int row = 0; row < rows; row++)
             {
-                for (int col = 0; col < 3; col++)
+                for (int col = 0; col < columns; col++)
                 {
                     State[row, col] = input[index];
                     if (input[index] == 0)
@@ -38,12 +40,12 @@ namespace UI_Zadanie1_c_Bukovska
             }
         }
 
-        public PuzzleNode(int[,] state, int[] spacePosition, List<MoveEnum> moves)
-            : this()
+        public PuzzleNode(int[,] state, int[] spacePosition, List<MoveEnum> moves, int rows, int columns)
+            : this(rows, columns)
         {
-            for (int i = 0; i < 3; i++)
+            for (int i = 0; i < rows; i++)
             {
-                for (int j = 0; j < 3; j++)
+                for (int j = 0; j < columns; j++)
                 {
                     State[i, j] = state[i, j];
                 }
@@ -64,7 +66,22 @@ namespace UI_Zadanie1_c_Bukovska
 
         public List<MoveEnum> GetPossibleMoves()
         {
-            return PossibleMoves.possibleMoves[(SpacePosition[0], SpacePosition[0])];
+            List<MoveEnum> possibleMoves = new List<MoveEnum>();
+
+            // left
+            if (this.SpacePosition[1] + 1 < this.Columns)
+                possibleMoves.Add(MoveEnum.Left);
+            // right
+            if (this.SpacePosition[1] - 1 >= 0)
+                possibleMoves.Add(MoveEnum.Right);
+            //up
+            if (this.SpacePosition[0] + 1 < this.Rows)
+                possibleMoves.Add(MoveEnum.Up);
+            //down
+            if (this.SpacePosition[0] - 1 >= 0)
+                possibleMoves.Add(MoveEnum.Down);
+
+            return possibleMoves;
         }
 
         public (int[,], int[]) GetNewState(MoveEnum move)
